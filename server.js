c0facc6f29ecf39d4835e1099984bfeefe53987d
VikramSadhya <vsadhy1@lsu.edu>
2016-07-19 11:36:17 -0500

    Testing google apis code

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var connect = require('connect');
 var path = require('path');
 var app = express();
 var bodyParser = require('body-parser');
+var google = require('googleapis');
+var proximitybeacon = google.proximitybeacon('v1beta1');
 
 var port = Number(process.env.PORT || 5000);
 
@@ -15,8 +17,11 @@ app.use(function(req, res, next) {
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static('public'));
 
+
 app.post('/postdata', function (req, res) {
-  res.send("Hello World");
+ proximitybeacon.beaconinfo.getforobserved("https://proximitybeacon.googleapis.com/v1beta1/beaconinfo:getforobserved?key=AIzaSyAru8xqtOm729RWjHjHN-6vL-9FhnEqDWI", function(data){
+  res.send(data);
+});
 });
 
 app.listen(port, function(err, req, res){
@@ -26,3 +31,4 @@ app.listen(port, function(err, req, res){
     console.log("Project app running at port:%s", port);
   }
 });
+
